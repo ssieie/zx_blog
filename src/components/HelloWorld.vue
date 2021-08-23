b911c3a68d5938edac72e153bed5252345e5323f
zhaoxin <iamzax@163.com>
2021-08-23 22:32:39 +0800

    zx

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template>
+  <div @click="testClick">{{ msg }}</div>
+  {{ test }}
+  {{ haha }}
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: Number,
+    test: {
+      type: Object,
+    },
+  },
+  emits: ["testEmit"],
+})
+export default class HelloWorld extends Vue {
+  public test!: object;
+  
+  public msg!: number;
+  
+  public haha: string = "ASD";
+
+  created() {
+    setTimeout(() => {
+      this.haha = "SSSSSSSS";
+    }, 2000);
+  }
+
+  testClick(): void {
+    this.$emit("testEmit", Math.floor(Math.random() * 10));
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
